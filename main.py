3037b748b2a85561bdd003616bc2546e43b5c072
petmas2008 <55663672+petmas2008@users.noreply.github.com>
2021-04-07 15:26:49 +0100

    added the high and low of the stock to the csv file

--- main.py
+++ main.py
@@ -8,25 +8,29 @@ tickerStrings = url_requests.run()
 # tickerStrings = ["TSCO"]
 
 def update_stock():
+    ticker_list = []
     open_list = []
     close_list = []
-    ticker_list = []
+    high_list = []
+    low_list = []
     for ticker in tickerStrings:
         data = yf.download(ticker, group_by="Ticker", period="1d", interval='1m')
         options = list(data)
         print(ticker, data)
         # data['Ticker'] = ticker  # add this column becasue the dataframe doesn't contain a column with the ticker
         if len(data) > 0:
-            open_list.append(data[options[0]][-1]) # index 0 refers to the open stock reference to the data
-            close_list.append(data[options[3]][-1]) # index 3 refers to the close stock reference to the data
             ticker_list.append(ticker)
-    save_data(ticker_list, open_list, close_list)
+            open_list.append(data[options[0]][-1]) # index 0 refers to the open of the stock
+            close_list.append(data[options[3]][-1]) # index 3 refers to the close of the stock
+            high_list.append(data[options[1]][-1]) # index 1 refers to the high of the stock
+            low_list.append(data[options[2]][-1]) # index 2 refers to the low of the stock
+    save_data(ticker_list, open_list, close_list, high_list, low_list)
 
-def save_data(ticker_list, open_list, close_list):
+def save_data(ticker_list, open_list, close_list, high_list, low_list):
     with open("ticker.csv", "a", newline="") as csvfile:
         spamwriter = csv.writer(csvfile)
-        for t, o, c in zip(ticker_list, open_list, close_list):
-            spamwriter.writerow([t, str(o), str(c)])
+        for t, o, c, h, l in zip(ticker_list, open_list, close_list, high_list, close_list):
+            spamwriter.writerow([t, str(o), str(c), str(h), str(l)])
             
 
 def show_data():
